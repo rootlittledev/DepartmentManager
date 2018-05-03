8c7876e48896c2043b951d293fc7e98d970f41d0
rootlittledev <qwentin1997@gmail.com>
2018-05-03 14:36:34 +0300

    Manager

--- src/main/java/DManager.java
+++ src/main/java/DManager.java
@@ -1,11 +1,81 @@
 import java.util.List;
 import java.util.Scanner;
 
-public class DManager {
+class DManager {
 
-    public static void main(String[] args){
+    private DBManager dbManager;
 
-        Menu menu = new Menu();
-        menu.drawMenu();
+    DManager() {
+        dbManager = new DBManager();
+    }
+
+    void addEmployee(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input name");
+        String name = scanner.nextLine();
+
+        System.out.println("Input assignment");
+        String assignment = scanner.nextLine();
+
+        System.out.println("Input departure name ");
+        String department = scanner.nextLine();
+
+        System.out.println("Input salary");
+        double salary = scanner.nextDouble();
+
+        dbManager.addEmployee(new Employee(name, department, assignment, salary));
+
+        System.out.println("Added successfully");
+    }
+
+    void getDepartmentHead(){
+        List<Employee> departmentHeads = dbManager.getDepartmentHead();
+
+        for (Employee employee: departmentHeads) {
+            System.out.println("Head of " + employee.getDepartment() + " department is " + employee.getName());
+        }
+    }
+
+    void getDepartmentStatistic(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input department's name");
+        String department = scanner.nextLine();
+
+        System.out.println("Assistants - " + dbManager.getPositionCount(department, "assistant"));
+        System.out.println("Associate professors - " + dbManager.getPositionCount(department, "associate professor"));
+        System.out.println("Professors - " + dbManager.getPositionCount(department, "professor"));
+    }
+
+    void getAverageSalary(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input department's name");
+        String department = scanner.nextLine();
+
+        System.out.println("The average salary of " + department + " is " + dbManager.getAverageSalary(department));
+    }
+
+    void getDepartmentCount(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input department's name");
+        String department = scanner.nextLine();
+
+        System.out.println(department + " has " + dbManager.getDepartmentCount(department) + " employees");
+    }
+
+    void globalSearch(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Search by: ");
+        String searchString = scanner.nextLine();
+
+        List<Employee> employees = dbManager.getEmployees(searchString);
+
+        for (Employee employee : employees) {
+            System.out.println(employee.getName() + " is working at " + employee.getDepartment());
+        }
     }
 }
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,12 +1,13 @@
-import java.util.List;
 import java.util.Scanner;
 
 class Menu {
 
-    DBManager dbManager;
+    private DBManager dbManager;
+    private DManager dManager;
 
     Menu() {
         dbManager = new DBManager();
+        dManager = new DManager();
     }
 
     void drawMenu(){
@@ -24,32 +25,32 @@ class Menu {
 
             switch (choice.nextInt()) {
                 case 1: {
-                    addEmployee();
+                    dManager.addEmployee();
 
                     break;
                 }
                 case 2: {
-                    getDepartmentHead();
+                    dManager.getDepartmentHead();
 
                     break;
                 }
                 case 3: {
-                    getDepartmentStatistic();
+                    dManager.getDepartmentStatistic();
 
                     break;
                 }
                 case 4: {
-                    getAverageSalary();
+                    dManager.getAverageSalary();
 
                     break;
                 }
                 case 5: {
-                    getDepartmentCount();
+                    dManager.getDepartmentCount();
 
                     break;
                 }
                 case 6: {
-                    globalSearch();
+                    dManager.globalSearch();
 
                     break;
                 }
@@ -59,77 +60,10 @@ class Menu {
                 }
                 default:
                     System.out.println("Wrong input!");
+                    return;
             }
         }
     }
 
-    private void addEmployee(){
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input name");
-        String name = scanner.nextLine();
-
-        System.out.println("Input assignment");
-        String assignment = scanner.nextLine();
-
-        System.out.println("Input departure name ");
-        String department = scanner.nextLine();
-
-        System.out.println("Input salary");
-        double salary = scanner.nextDouble();
-
-        dbManager.addEmployee(new Employee(name, department, assignment, salary));
-
-        System.out.println("Added successfully");
-    }
-
-    private void getDepartmentHead(){
-        List<Employee> departmentHeads = dbManager.getDepartmentHead();
-
-        for (Employee employee: departmentHeads) {
-            System.out.println("Head of " + employee.getDepartment() + " department is " + employee.getName());
-        }
-    }
-
-    private void getDepartmentStatistic(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Input department's name");
-        String department = scanner.nextLine();
-
-        System.out.println("Assistants - " + dbManager.getPositionCount(department, "assistant"));
-        System.out.println("Associate professors - " + dbManager.getPositionCount(department, "associate professor"));
-        System.out.println("Professors - " + dbManager.getPositionCount(department, "professor"));
-    }
-
-    private void getAverageSalary(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Input department's name");
-        String department = scanner.nextLine();
-
-        System.out.println("The average salary of " + department + " is " + dbManager.getAverageSalary(department));
-    }
-
-    private void getDepartmentCount(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Input department's name");
-        String department = scanner.nextLine();
-
-        System.out.println(department + " has " + dbManager.getDepartmentCount(department) + " employees");
-    }
-
-    private void globalSearch(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Search by: ");
-        String searchString = scanner.nextLine();
-
-        List<Employee> employees = dbManager.getEmployees(searchString);
-
-        for (Employee employee : employees) {
-            System.out.println(employee.getName() + " is working at " + employee.getDepartment());
-        }
-    }
 }
